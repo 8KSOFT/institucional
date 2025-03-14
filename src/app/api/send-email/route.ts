f69548455bb8791758ea30c6bde3880f96dc2ee5
lucopdev <lucopdev@gmail.com>
2025-03-14 09:23:56 -0300

    corrigido envio de contato

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -5,14 +5,13 @@ const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(request: NextRequest) {
   try {
-    console.log(process.env.NEXT_PUBLIC_RESEND_API_KEY);
     const body = await request.json();
-    console.log(body);
+
     const { name, email, message } = body;
 
     const { data, error } = await resend.emails.send({
-      from: '8KSOFT <contato@8ksoft.com>',
-      to: ['destinatario@8ksoft.com.br'],
+      from: '8KSOFT Site <noreply@8ksoft.com>',
+      to: ['contato@8ksoft.com'],
       subject: 'Nova Mensagem de Contato - Site',
       html: `
         <h1>Nova Mensagem de Contato</h1>
